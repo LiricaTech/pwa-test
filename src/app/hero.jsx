96f2adf8aca644ba9b0191650a0766ba0bd71b3b
Esteban Arce <eesteban.arce@gmail.com>
2023-12-22 09:45:06 -0300

    styles update again

--- src/app/hero.jsx
+++ src/app/hero.jsx
@@ -6,7 +6,7 @@ function Hero({img = "https://westonacademy.education/assets/imgs/logo-WIA.png",
     <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
       {title ? <h1>{title}</h1> : "Default Title"}
       {img && <Image src={img} alt="hero" width={300} height={200} />}
-      <button>Log In</button>
+      <button style={{padding: 20, margin: 20, borderRadius: 10, backgroundColor: "darkblue", color: "white", border: "none", cursor: "pointer", fontSize: 20, fontWeight: "bold"}}>Log In</button>
     </div>
   )
 }
